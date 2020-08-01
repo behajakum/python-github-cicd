6e25b266347a9e396c26b7088c7dd5daae5faf45
ajaykbehera <beheraajay@yahoo.com>
2020-08-01 08:56:38 +0530

    app file created

--- app/app.py
+++ app/app.py
@@ -2,3 +2,14 @@
 https://github.com/behajakum/python-github-cicd
 sample flask hello world to demonstrate cicd with github actions
 """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1> Hello World! </h1>"
+
+if __name__ == "__main__":
+    app.run()
